7cf07619a4fddea6040377eac1b8d52d3127bb5c
ScottieLee <scottcmon@gmail.com>
2015-12-21 21:22:13 +0800

    5. Longest Palindromic Substring

---
+++ 5_Longest_Palindromic_Substring.playground/Contents.swift
@@ -0,0 +1,40 @@
+//: 5. Longest Palindromic Substring
+
+
+/*
+Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+*/
+
+
+/// DP Solution
+
+func findLongestPalindromicSubStringWithString(str: String) -> String {
+    var dp = [[Bool]](count: str.characters.count, repeatedValue: [Bool](count: str.characters.count, repeatedValue: false))
+    var maxGap = 0
+    var retI = 0
+    var retJ = 0
+    for i in (0 ... str.characters.count - 1).reverse() {
+        for j in (0 ... str.characters.count - 1).reverse() {
+            if i >= j {
+                dp[i][j] = true
+                continue
+            }
+            if i < str.characters.count - 1 && j >= 1 {
+                if dp[i+1][j-1] == true && str[str.startIndex.advancedBy(i)] == str[str.startIndex.advancedBy(j)] {
+                    dp[i][j] = true
+                    if j - i > maxGap {
+                        maxGap = j - i
+                        retI = i
+                        retJ = j
+                    }
+                    continue
+                }
+            }
+        }
+    }
+    print("\(retI), \(retJ)")
+    return str[str.startIndex.advancedBy(retI) ... str.startIndex.advancedBy(retJ)]
+}
+
+let string = "abcdfeggefdcxurkeekrabc"
+print("\(findLongestPalindromicSubStringWithString(string))")
